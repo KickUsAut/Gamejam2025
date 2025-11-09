e07b2d4d422b2a7ef1143c269228f018f5d28259
Florentin <florentin@MacBook-Pro-von-Florentin.local>
2025-11-09 20:42:55 +0100

    shadowmoving

--- GameJam_Timeless/Assets/Scripts/ItemCollector.cs
+++ GameJam_Timeless/Assets/Scripts/ItemCollector.cs
@@ -3,15 +3,29 @@ using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
-    public int itemCount;
-    public TMP_Text itemText;
+    public int score = 0;
+    public int pointsPerItem = 100;
+    private TMP_Text scoreText;
+    
+    private float cooldownTime = 0.1f;
+    private float lastCollectTime = -1f;
+
+    void Start()
+    {
+        // Text automatisch finden, falls nicht zugewiesen
+        if (scoreText == null)
+        {
+            scoreText = GameObject.Find("ScoreText").GetComponent<TMP_Text>();
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Item"))
+        if (other.CompareTag("Item") && Time.time > lastCollectTime + cooldownTime)
         {
-            itemCount++;
-            itemText.text = itemCount.ToString();
+            score += pointsPerItem;
+            scoreText.text = score.ToString();
+            lastCollectTime = Time.time;
             Destroy(other.gameObject);
         }
     }
